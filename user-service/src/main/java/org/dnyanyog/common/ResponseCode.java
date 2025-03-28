5766cf4169e5b7c64c2fb704f53c1ee0d2a394a6
Himanshh20 <137694881+Himanshh20@users.noreply.github.com>
2025-03-28 23:43:24 +0530

    feat: Implement JWT token generation and validation
     * Implemented token generation, validation, and claim extraction
     * Implemented JWT authentication with BCrypt password hashing

--- user-service/src/main/java/org/dnyanyog/common/ResponseCode.java
+++ user-service/src/main/java/org/dnyanyog/common/ResponseCode.java
@@ -7,7 +7,8 @@ public enum ResponseCode {
   Delete_User("User Deleted Sucessfully ", "200"),
   User_Not_Found("User Not Found ", "404"),
   Sucess("Login Successful", "200"),
-  FAILURE("Invalid Username or Password", "404");
+  FAILURE("Invalid Username or Password", "404"),
+  PASSWORD_RESET("Password Reset Sucessfully", "404");
 
   private final String code;
   private final String message;
