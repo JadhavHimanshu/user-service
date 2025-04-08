dd47e964a18018effa55719dbbd2d3b39670c587
Himanshh20 <137694881+Himanshh20@users.noreply.github.com>
2025-04-08 22:08:12 +0530

    feat: Implement JWT token generation and validation
     * Implemented token generation, validation, and claim extraction
     * Implemented JWT authentication with BCrypt password hashing

---
+++ user-service/src/test/java/org/dnyanyog/controller/UserServiceController.java
@@ -0,0 +1,104 @@
+package org.dnyanyog.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status; // For
+
+import org.dnyanyog.common.ResponseCode;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.jayway.jsonpath.JsonPath;
+
+public class UserServiceController {
+
+  @Autowired MockMvc mockMvc;
+
+  @Test
+  public void addUser() throws Exception {
+    RequestBuilder requestBuilder =
+        MockMvcRequestBuilders.post("/api/v1/auth/user")
+            .content(
+                "{\r\n"
+                    + "  \"userId\": 101,\r\n"
+                    + "  \"name\": \"Himanshu Jadhav\",\r\n"
+                    + "  \"userName\": \"himanshu12234\",\r\n"
+                    + "  \"password\": \"SecurePass@123\",\r\n"
+                    + "  \"confirmPassword\": \"SecurePass@123\",\r\n"
+                    + "  \"passswordExpiry\": \"2025-12-31\",\r\n"
+                    + "  \"userRole\": \"ADMIN\",\r\n"
+                    + "  \"userStatus\": \"ACTIVE\", \r\n"
+                    + "  \"tenant\" : \"Java\",\r\n"
+                    + "  \"department\": \"IT\",\r\n"
+                    + "  \"tenantId\": \"TEN12345\"\r\n"
+                    + "\r\n"
+                    + "}")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON);
+    mockMvc
+        .perform(requestBuilder)
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.code").value(ResponseCode.Add_User.getCode()))
+        .andExpect(jsonPath("$.message").value(ResponseCode.Add_User.getMessage()))
+        .andReturn();
+  }
+
+  @Test
+  public void updateUser() throws Exception {
+    RequestBuilder requestBuilder =
+        MockMvcRequestBuilders.post("")
+            .content("")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON);
+
+    mockMvc
+        .perform(requestBuilder)
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.code").value(ResponseCode.Update_User.getCode()))
+        .andExpect(jsonPath("$.message").value(ResponseCode.Update_User.getMessage()))
+        .andReturn();
+  }
+
+  @Test
+  public void serchByID() throws Exception {
+    RequestBuilder requestBuilder =
+        MockMvcRequestBuilders.get("/api/v1/auth/user_seacrh/{userId)}");
+    mockMvc
+        .perform(requestBuilder)
+        .andExpect(jsonPath("$.code").value(ResponseCode.Search_User.getCode()))
+        .andExpect(jsonPath("$.message").value(ResponseCode.Search_User.getMessage()))
+        .andReturn();
+  }
+
+  @Test
+  public void changePassword() throws Exception {
+    RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/v1/auth/change_password");
+    mockMvc
+        .perform(requestBuilder)
+        .andExpect(jsonPath("$code").value(ResponseCode.PASSWORD_RESET.getCode()))
+        .andExpect(jsonPath("$.message").value(ResponseCode.PASSWORD_RESET.getMessage()))
+        .andReturn();
+  }
+
+  @Test
+  public void getAllUsers() throws Exception {
+    RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/v1/auth/users");
+    mockMvc
+        .perform(requestBuilder).andExpect(jsonPath("$.code")))
+        .andExpect(jsonPath("$code").value(ResponseCode.Search_User.getCode()))
+        .andExpect(jsonPath("$.message").value(ResponseCode.Search_User.getMessage()))
+        .andReturn();
+  }
+
+  @Test
+  public void deleteByID() throws Exception {
+    RequestBuilder requestBuilder =
+        MockMvcRequestBuilders.delete("/api/v1/user/user_delete/{userId}");
+       mockMvc
+        .perform(requestBuilder)
+      .andExpect(jsonPath("$code").value(ResponseCode.Delete_User.getCode())).andExpect(JsonPath("$message").value(ResponseCode.Delete_User.getMessage())).andReturn();
+ 
+  }
+}
